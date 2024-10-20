abb1f6f1a89c44df6ec0a03099b6f64982251ec0
Danil <daniltro1@yandex.ru>
2024-10-20 20:29:03 +0300

    feat: added footer and full desktop version

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
-import './styles/maingcss'
+import "./styles/main.css";
+console.log("hello world");
 
+const submitButton = document.querySelector(".subscribe-form__button");
+const beforeSubmitBlock = document.querySelector(".subscribe-form__controls-wrapper");
+const afterSubmitBlock = document.querySelector(
+  ".subscribe-form__success-submit"
+);
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  beforeSubmitBlock.classList.add("inactive");
+  afterSubmitBlock.classList.add("active");
+});
