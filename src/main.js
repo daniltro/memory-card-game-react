22a263cadbcd5fa4b533ca78aa17b1a2a0373c7d
Danil <daniltro1@yandex.ru>
2024-10-22 00:41:13 +0300

    feat: added tablet version

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import "./styles/main.css";
 console.log("hello world");
 
 const submitButton = document.querySelector(".subscribe-form__button");
-const beforeSubmitBlock = document.querySelector(".subscribe-form__controls-wrapper");
+const beforeSubmitBlock = document.querySelector(
+  ".subscribe-form__controls-wrapper"
+);
+
 const afterSubmitBlock = document.querySelector(
   ".subscribe-form__success-submit"
 );
@@ -10,5 +13,21 @@ const afterSubmitBlock = document.querySelector(
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
   beforeSubmitBlock.classList.add("inactive");
+  // afterSubmitBlockWrapper.add("active");
   afterSubmitBlock.classList.add("active");
+});
+
+// ------------------------------------------
+
+const contentArr = Array.from(
+  document.querySelectorAll(".creeping-line__content")
+);
+
+let screenWidth = window.innerWidth;
+
+contentArr.forEach((content) => {
+  // Дублируем содержимое до тех пор, пока его ширина не превысит ширину экрана в 2 раза
+  while (content.offsetWidth < screenWidth * 2) {
+    content.innerHTML += content.innerHTML;
+  }
 });
