5da9c6e42cd09dc47ea56914b4d1ef799fca3da0
Danil <daniltro1@yandex.ru>
2024-10-23 06:36:14 +0300

    feat: added mobile version

--- src/main.js
+++ src/main.js
@@ -1,33 +1,73 @@
 import "./styles/main.css";
-console.log("hello world");
 
 const submitButton = document.querySelector(".subscribe-form__button");
+const emailInput = document.querySelector(".subscribe-form__input");
+const checkboxInput = document.querySelector(".subscribe-form__input-checkbox");
 const beforeSubmitBlock = document.querySelector(
   ".subscribe-form__controls-wrapper"
 );
-
 const afterSubmitBlock = document.querySelector(
   ".subscribe-form__success-submit"
 );
+const errorMessage = document.querySelector(".error-message");
 
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
-  beforeSubmitBlock.classList.add("inactive");
-  // afterSubmitBlockWrapper.add("active");
-  afterSubmitBlock.classList.add("active");
+
+  if (emailInput.checkValidity() && checkboxInput.checked) {
+    beforeSubmitBlock.classList.add("inactive");
+    afterSubmitBlock.classList.add("active");
+  } else {
+    if (!checkboxInput.checked) {
+      errorMessage.textContent =
+        "Por favor, acepte los términos y la política de privacidad.";
+
+      errorMessage.style.display = "block";
+    }
+  }
 });
 
 // ------------------------------------------
 
 const contentArr = Array.from(
-  document.querySelectorAll(".creeping-line__content")
+  document.querySelectorAll(".marquee-line__content")
 );
 
 let screenWidth = window.innerWidth;
 
 contentArr.forEach((content) => {
-  // Дублируем содержимое до тех пор, пока его ширина не превысит ширину экрана в 2 раза
   while (content.offsetWidth < screenWidth * 2) {
     content.innerHTML += content.innerHTML;
   }
+});
+
+// ---------------------------------
+
+let lastScrollTop = 0;
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > lastScrollTop) {
+    header.classList.add("header--hidden");
+  } else {
+    header.classList.remove("header--hidden");
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Ограничиваем значение, чтобы не было отрицательного
+});
+
+// ------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", function () {
+  const accordions = document.querySelectorAll(".accordion");
+
+  accordions.forEach((accordion) => {
+    accordion.addEventListener("click", function () {
+      this.classList.toggle("active");
+
+      const content = this.parentElement;
+      content.classList.toggle("active");
+    });
+  });
 });
