4d3e78c61628e32413fc8fbd4dd6e6d860b88a7f
Danil <daniltro1@yandex.ru>
2024-11-20 23:38:01 +0300

    feat: добавлен запрос карточек и их отображение на странице

---
+++ src/components/app/app.tsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+
+async function fetchImage(seed: string) {
+  const url = `https://api.dicebear.com/9.x/pixel-art/svg?seed=${seed}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const svgText = await response.text();
+    return svgText;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+    return null;
+  }
+}
+
+const svgToBase64 = (svg: string) => {
+  return `data:image/svg+xml;base64,${window.btoa(unescape(encodeURIComponent(svg)))}`;
+};
+
+// Если нужно чтобы карточки были рандомные каждый раз.
+const generateRandomSeed = () => {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let seed = '';
+  for (let i = 0; i < 8; i++) {
+    seed += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return seed;
+};
+
+const App = () => {
+  const [cards, setCards] = useState<string[]>([]);
+  console.log(cards);
+
+  useEffect(() => {
+    const fetchAllImages = async () => {
+      // const seeds = [
+      //   'John',
+      //   'Alice',
+      //   'Charlie',
+      //   'Bob',
+      //   'Dany',
+      //   'Martin',
+      //   'Judi',
+      //   'Ashley',
+      // ];
+
+      // Генерируем случайные seed
+      const seeds = Array.from({ length: 8 }, () => generateRandomSeed());
+
+      const fetchedCards = await Promise.all(
+        seeds.map((seed) => fetchImage(seed)),
+      );
+      setCards(fetchedCards as string[]);
+    };
+
+    fetchAllImages();
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Memory cards</h1>
+      <div className="cards-container">
+        {cards.length > 0 ? (
+          cards.map((svg, index) => (
+            <img
+              className="card"
+              key={index}
+              src={svgToBase64(svg)}
+              alt={`Card ${index}`}
+            />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import './styles/main.css';
+import App from './components/app/app';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
+root.render(<App />);
