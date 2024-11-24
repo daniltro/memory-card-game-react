a79bfdf17635a6f64a0aff1c928d318162db77ee
Danil <daniltro1@yandex.ru>
2024-11-24 20:39:33 +0300

    fix: исправлено название папки с компонентом App 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import './styles/main.css';
-import App from './components/App/app';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
 root.render(<App />);
