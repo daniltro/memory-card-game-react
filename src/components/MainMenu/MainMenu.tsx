5188268f10e78fdb06859568b19a736296bbcb2e
Danil <daniltro1@yandex.ru>
2024-11-24 15:23:13 +0300

    feat: добавлена стилизация стартовой страницы

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 export default function MainMenu() {
   return (
     <div className="main-menu">
-      <h1 className="playing-field__title"> Игра &quot;Запомни пары&quot;</h1>
+      <h1 className="main-menu__title">
+        <span>Игра</span> &quot;Запомни пары&quot;
+      </h1>
 
       <nav className="main-menu__nav">
         <ul className="main-menu__list">
@@ -13,17 +15,17 @@ export default function MainMenu() {
               Новая игра
             </Link>
           </li>
-          <li className="main-menu__list">
+          <li className="main-menu__list-item">
             <Link className="main-menu__link" to="/settings">
               Настройки
             </Link>
           </li>
-          <li className="main-menu__list">
+          <li className="main-menu__list-item">
             <Link className="main-menu__link" to="/results">
               Результаты
             </Link>
           </li>
-          <li className="main-menu__list">
+          <li className="main-menu__list-item">
             <Link className="main-menu__link" to="/rules">
               Правила
             </Link>
